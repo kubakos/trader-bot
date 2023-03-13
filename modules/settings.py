2bc4cdf42fd4df723d7ae9ffafe63f693a2c66f0
Akos Kubak <kubakakos@gmail.com>
2023-03-13 16:15:50 +0100

    Initial commit

--- modules/settings.py
+++ modules/settings.py
@@ -1,3 +1,3 @@
-my_oanda_token = '***'
-oanda_api_url = 'https://api-fxtrade.oanda.com'
-oanda_account_id = '***'
+my_oanda_token = ''
+oanda_api_url = 'https://api-fxtrade-practice.oanda.com'
+oanda_account_id = ''
